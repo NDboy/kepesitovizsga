973c517bc7956f67a3566c44670f3c1913b146f8
NDboy <endre.karsai@gmail.com>
2021-04-29 12:05:28 +0200

    05

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/army/Army.java
@@ -0,0 +1,49 @@
+package hu.nive.ujratervezes.kepesitovizsga.army;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Army {
+
+    private List<MilitaryUnit> militaryUnits = new ArrayList<>();
+
+    public void addUnit (MilitaryUnit militaryUnit) {
+        militaryUnits.add(militaryUnit);
+    }
+
+    public void damageAll(int damage) {
+        militaryUnits.forEach(a -> a.sufferDamage(damage));
+        militaryUnits = militaryUnits.stream()
+                .filter(a -> a.getHitPoints() >= 25)
+                .collect(Collectors.toList());
+    }
+
+    public int getArmyDamage() {
+        return militaryUnits.stream()
+                .mapToInt(MilitaryUnit::doDamage)
+                .sum();
+
+    }
+
+    public int getArmySize() {
+        return militaryUnits.size();
+    }
+}
+
+//    Sereg (Collections feladat, 20 pont, tesztosztály: ArmyTest)
+//    A hadseregnek az alábbi metódusai vannak:
+//
+//        addUnit(MilitaryUnit militaryUnit),
+//        amivel egy katonai egységet tudunk hozzáadni a sereghez,
+//
+//        damageAll(int damage), ami a sereg minden
+//        egységének életerejéből levonja a paraméterül kapott értéket,---------------------------------
+//        valamint eltávolítja a seregből azokat az egységeket,
+//        melyeknek így 25 pont alá csökkent az életerejük (azaz harcképtelenek vagy halottak),----------------
+//
+//        getArmyDamage(), ami visszaadja,
+//        hogy az aktuális támadással mennyi sebzést okoz a sereg összesen,
+//
+//        getArmySize(), ami visszaadja, hogy hány egységből áll a sereg.
